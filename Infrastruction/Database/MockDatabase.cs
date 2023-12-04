3e45e15dbc54c180f08947df3baa6b413f09907f
lokakos01 <114986974+lokakos01@users.noreply.github.com>
2023-12-04 13:10:01 +0100

    l

--- Infrastruction/Database/MockDatabase.cs
+++ Infrastruction/Database/MockDatabase.cs
@@ -27,5 +27,29 @@ namespace Infrastruction.Database
                 AnimalId = Guid.NewGuid(), Name = "Kalle"
             },
         };
+
+        public List<Cat> allCats
+        {
+            get { return AllCatsFromMockDatabase; }
+            set { AllCatsFromMockDatabase = value; }
+
+        }
+
+        private static List<Dog> AllDogsFromMockDatabase = new()
+        {
+            new Dog
+            {
+                AnimalId = Guid.NewGuid(), Name = "Dragan"
+            }, new Dog
+            {
+                AnimalId = Guid.NewGuid(), Name = "Zlatan"
+            }, new Dog
+            {
+                AnimalId = Guid.NewGuid(), Name = "Ibra"
+            }, new Dog
+            {
+                AnimalId = Guid.NewGuid(), Name = "Kalle"
+            },
+        };
     }
 }
